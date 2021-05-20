9d1eadcd1bb44f660dd178e90f64d500890c5106
AbhijithGanesh14122003@gmail.com <g.abhijith14122003@gmail.com>
2021-05-20 13:26:51 +0530

    Changes to settings and requirements.txt

--- WorkingApp/urls.py
+++ WorkingApp/urls.py
@@ -2,7 +2,10 @@
 from django.urls import path
 from WorkingApp.views import LandingPage
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', LandingPage)
 ]
+
+urlpatterns.append(staticfiles_urlpatterns)
