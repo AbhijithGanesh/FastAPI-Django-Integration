34eab80c4f669d24a9720263e569e3b2a3465d62
Abhijith Ganesh <g.abhijtih14122003@gmail.com>
2021-05-20 18:17:08 +0530

    Changes to urls.py🚀

--- TestingProject/urls.py
+++ TestingProject/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from .views import On_Landing
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = [
+urlpatterns = patterns(
     path('admin/', admin.site.urls),
     path('', On_Landing),
     path('Project/', include('WorkingApp.urls'))
-]
+)
 
 urlpatterns+staticfiles_urlpatterns()
