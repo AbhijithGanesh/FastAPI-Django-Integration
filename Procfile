6cdb4c34f23cd8f120b5b88b5f25fa66470367b5
Abhijith Ganesh <g.abhijtih14122003@gmail.com>
2021-05-20 11:01:55 +0530

    Changes to settings-->Testing deployment at Heroku

---
+++ Procfile
@@ -0,0 +1,4 @@
+python-3.9.4
+web: uvicorn TestingProject.asgi --log-file-
+python manage.py makemigrations
+python manage.py migrate
--- TestingProject/settings.py
+++ TestingProject/settings.py
@@ -1,5 +1,6 @@
-
 import os
+
+PROJECT_NAME = 'Integrating FastAPI with Django'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
@@ -12,6 +13,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "api",
+    'WorkingApp'
 ]
 
 MIDDLEWARE = [
@@ -54,6 +56,38 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
@@ -64,4 +98,3 @@ TIME_ZONE = 'Asia/Calcutta'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-PROJECT_NAME = 'Integrating FastAPI with Django'
