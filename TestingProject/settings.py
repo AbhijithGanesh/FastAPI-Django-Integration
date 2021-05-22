bde957014fe7b964bf828c8142ce70402543bd41
Abhijith Ganesh <g.abhijtih14122003@gmail.com>
2021-05-22 14:33:35 +0530

    Changing Settings⚙'

--- TestingProject/settings.py
+++ TestingProject/settings.py
@@ -2,6 +2,7 @@ import os
 PROJECT_NAME = 'Integrating FastAPI with Django'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = 'secret'
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1','localhost', 'fastapi-over-django.herokuapp.com', '*']
 INSTALLED_APPS = [
