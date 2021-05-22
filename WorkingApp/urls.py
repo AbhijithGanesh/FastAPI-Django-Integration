2204dd9d436064cb26708f709509c62277e1f1a8
Abhijith Ganesh <g.abhijtih14122003@gmail.com>
2021-05-22 12:18:13 +0530

    Miscellanous changes❓

--- WorkingApp/urls.py
+++ WorkingApp/urls.py
@@ -2,10 +2,9 @@
 from django.urls import path
 from WorkingApp.views import LandingPage
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', LandingPage)
 ]
 
-urlpatterns.append(staticfiles_urlpatterns())
+
